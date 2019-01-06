191f592d3abd7170423388d7beb4e3bf43f80df9
Daniel YU <i@icymint.me>
2019-01-06 12:00:26 +0800

    fix tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,15 +4,8 @@
 
 module Main where
 
-
-import           Control.Monad.IO.Class
-import           Data.Aeson
-import           Data.Maybe
-import qualified Data.Salak              as S
-import           Data.Text               (Text)
 import qualified Data.Text               as T
 import           Test.Hspec
-import           Test.QuickCheck
 import           Test.QuickCheck.Monadic
 import           Yam
 
@@ -23,16 +16,6 @@ spec = do
   describe "Yam.Config" specConfig
 
 specConfig = do
-  context "configuration" $ do
-    it "load" $ do
-      p <- S.defaultPropertiesWithFile "yam_test.yml"
-      let get :: S.FromProperties a => Text -> Maybe a
-          get = flip S.lookup p
-      -- let dsf = get "yam.datasource" :: Maybe DataSourceConfig
-      -- shouldSatisfy dsf isJust
-      let conf = get "yam.application" :: Maybe AppConfig
-      shouldSatisfy conf isJust
-      -- datasource <$> conf `shouldBe` dsf
   context "randomTest" $ do
     it "random" $ do
       monadicIO $ do
